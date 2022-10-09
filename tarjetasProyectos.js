10b66a8d48fa6d4c9f2dfefe3e5dabbbd9710ef5
brayanjavier329 <bjninoa@correo.udistrital.edu.co>
2022-10-08 23:44:57 -0500

    hkhd

--- tarjetasProyectos.js
+++ tarjetasProyectos.js
@@ -1,7 +1,7 @@
 /*const personajes=fetch("https://rickandmortyapi.com/api/character");
 personajes.then(response => response.json())
 console.log(personajes) */
-function getCharacters(done){
+/*function getCharacters(done){
     const results=fetch("https://rickandmortyapi.com/api/character");
 
     results
@@ -33,34 +33,23 @@ getCharacters(data => {
 
         main.append(article)
     });
-})
-console.log(getCharacters(done))
-/*
-var proyecto=[{
-    "id":1,
-    "titulo":"Salvando arboles",
-    "descripcion":"Se siembran nuevos arboles",
-    "direccionWallet":"AJGUG43UG43",
-    "img":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tooltyp.com%2F8-beneficios-de-usar-imagenes-en-nuestros-sitios-web%2F&psig=AOvVaw0in_e_kkkKE8gy03YnfJto&ust=1665372662738000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNjzzuKa0voCFQAAAAAdAAAAABAD"
-},{
-    "id":2,
-    "titulo":"Adoptando perritos",
-    "descripcion":"Brindandoles un nuevo hogar",
-    "direccionWallet":"OURIOEU587859",
-    "img":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dogalize.com%2Fes%2F2017%2F06%2Ffotos-de-perros-cachorros-imagenes%2F&psig=AOvVaw1LznObWGwUQYjjcSOqazIP&ust=1665369876650000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNjC-7GQ0voCFQAAAAAdAAAAABAD"
-}]
+    console.log(data)
+})*/
+
+
+
 
 console.log(proyecto)
 
 function dibujarTarjetas(){
-    for (let i = 0; i < Object.keys(proyecto).length; i++) {
+    for (let i = 0; i < Object.keys(prueba.json).length; i++) {
         const tarjeta=document.createRange().createContextualFragment(`
     <div class="row mx-5">
         <div class="col">
             <div class="card" style="width: 18rem;">
                 <img src="${proyecto.img}>
                 <div class="card-body">
-                    <h1>${proyecto.titulo}</h1>
+                    <h1>${(prueba.json).titulo}</h1>
                     <p>${proyecto.descripcion}
                     </p>
                     <a href="infoProject.html" class="btn btn-primary">Info del project</a>
@@ -76,4 +65,4 @@ function dibujarTarjetas(){
         main.append(tarjeta)
       }
 }
-dibujarTarjetas()*/
+dibujarTarjetas()
